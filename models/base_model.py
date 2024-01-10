87fed9d641d32d3c65c21c35c4f77500fff27f05
mohammed shanan <mohammedshanan12@gmail.com>
2024-01-10 14:25:06 +0200

    fixed id

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import uuid
 from datetime import datetime
 class BaseModel():
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
     def __str__(self):
@@ -25,7 +25,14 @@ class BaseModel():
                 new_dict[key] = x
         return new_dict
 
-test = BaseModel()
-# print(test)
-print(test.to_dict())
-print(test.id)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
